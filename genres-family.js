eb907360381228e16000f39ba3ecdf39cb1ecf00
Julien G <molokoloco@gmail.com>
2017-11-23 16:27:15 +0100

    first commit

---
+++ genres-family.js
@@ -0,0 +1,37 @@
+const genres = {
+	"Film": [
+		"Horeur", "Horreur", "Espionnage", "Comédie policière", "Suspense", "Fantastique", "Policier", "Western", "Cinéma", "Science-fiction", "Comédie dramatique", "Comédie musicale", "Histoire", "Biographie", "Téléfilm", "Drame", "Comédie", "Action", "Court métrage"
+	],
+	"Série": [
+		"Horeur", "Horreur", "Espionnage", "Comédie policière", "Suspense", "Fantastique", "Policier", "Western", "Science-fiction", "Comédie dramatique", "Comédie musicale", "Histoire", "Biographie", "Drame", "Comédie", "Action", "Animation"
+	],
+	"Sport": [
+		"Patinage artistique", "Sports d'hiver", "Formule 1", "Equitation", "Kick-boxing", "Biathlon", "Athlétisme", "Volley-ball", "Cyclo-cross", "Cyclisme", "Ski", "Ski freestyle", "Snowboard", "Catch", "Kiteboard", "Tennis", "NASCAR", "Hippisme", "Précision", "Pétanque", "Mécanique", "Rallycross", "Automobilisme", "Cesta punta", "Golf", "Hockey sur glace", "Surf", "Nautisme", "Sportsnautiques", "Motonautisme", "Canoë-kayak", "Sports nautiques", "Fun", "Sports de combat", "Boxe", "Handball", "Rugby", "Par équipes", "Football américain", "Voile", "Basket-ball", "Fitness", "Football", "Multisports", "e-sport"
+	],
+	"Information": [
+		"Emploi", "Médias", "Economie", "Santé", "Débat parlementaire", "Débat", "Politique", "Journal", "Interview","Justice"
+	],
+	"Documentaire": [
+		"Civilisations", "Société", "Nature", "Animalier"
+	],
+	"Culture": [
+		"Beaux-arts", "Conte", "Ballet contemporain", "Ballet", "Ballet classique", "Pièce de théâtre", "Théâtre", "Café-théâtre", "Opéra", "Mode", "Religions", "Sciences et technique"
+	],
+	"Divertissement": [
+		"Sketches", "Poker", "Cirque", "Cérémonie", "One man show", "Décoration", "Loterie", "Talk show", "Jardinage", "Gastronomie", "Télé-achat", "Téléréalité", "Art de vivre", "Jeu", "Spectacle", "Découvertes", "Humour", "Loisirs"
+	],
+	"Musique": [
+		"Concert", "Rock-pop", "Pop & Rock", "Rap & Techno", "Oratorio", "Jazz", "Musique classique", "Variétés", "Musicale", "Classique", "Clips"
+	],
+	"Jeunesse": [
+		"Animation", "Manga", "Dessin animé"
+	],
+	"Charme": [
+		"Pornographie", "Erotisme"
+	],
+	"Autre": [
+		"Education", "Spéciale", "Voyance", "A découvrir", "Consommation", "Fiction", "Météo", "Programme indéterminé", "Programme", "Services", "Fin", "Magazine"
+	]
+}
+
+export default genres;
